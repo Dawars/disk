13c4b07ecd729f5d9a6a1a354ede29a03a4e2ce4
Dávid Komorowicz <dawars00@gmail.com>
2024-10-10 18:26:31 +0200

    Eval after each batch

--- train.py
+++ train.py
@@ -66,7 +66,7 @@ def train_model(args):
     trainer = pl.Trainer(devices=args.num_gpus,
                          num_nodes=args.num_nodes,
                          log_every_n_steps=50, #cfg.TRAINING.LOG_INTERVAL,
-                         val_check_interval=5000, #cfg.TRAINING.VAL_INTERVAL,
+                         val_check_interval=1.0, #cfg.TRAINING.VAL_INTERVAL,
                          limit_val_batches=100,#cfg.TRAINING.VAL_BATCHES,
                          # limit_train_batches=args.chunk_size,
                          max_epochs=args.num_epochs,
